028f70f81e9a7756ef210d215fdfbc8b4c88c916
shankar thakur <shankarthaur07@gmail.com>
2024-07-01 11:29:45 +0530

    inital commit

---
+++ src/main/java/com/supreme/TaskSubmissionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.supreme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSubmissionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSubmissionServiceApplication.class, args);
+	}
+
+}
